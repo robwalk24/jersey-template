39db030ef89ba1c0ef37ef7051117019f5290161
Robert Walker <walkerra@gmail.com>
2018-05-06 12:55:45 -0500

    added mapper for WebApplicationException

--- src/main/java/io/rowtech/api/exception/mapper/WebApplicationExceptionMapper.java
+++ src/main/java/io/rowtech/api/exception/mapper/WebApplicationExceptionMapper.java
@@ -3,6 +3,7 @@ package io.rowtech.api.exception.mapper;
 import io.rowtech.api.model.ErrorModel;
 
 import javax.inject.Singleton;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -10,11 +11,11 @@ import javax.ws.rs.ext.Provider;
 
 @Provider
 @Singleton
-public class ThrowableExceptionMapper implements ExceptionMapper<Throwable> {
+public class WebApplicationExceptionMapper implements ExceptionMapper<WebApplicationException> {
     @Override
-    public Response toResponse(Throwable exception) {
+    public Response toResponse(WebApplicationException exception) {
         return Response
-                .status(Response.Status.INTERNAL_SERVER_ERROR)
+                .status(exception.getResponse().getStatus())
                 .type(MediaType.APPLICATION_JSON)
                 .entity(new ErrorModel(exception.getMessage()))
                 .build();
